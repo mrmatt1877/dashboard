4e69cf9c2e9068451c37b9833f4a66353b9e30d7
Matt <mrmatt1877@gmail.com>
2017-02-09 20:27:17 +0000

    json data is now feeding the d3 graph. Making adjustments to graph display

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -13,80 +13,77 @@
 
 function draw(json_data) {
     
-    
-    var oranges2 =[];
-    for (var i = 0; i < json_data.length; i++){
-        var t = json_data[i].start_time.split(/[- T : Z]/);
-        var t2 = json_data[i].end_time.split(/[- T : Z]/);
-
-        // Apply each element to the Date function
-        var secondsTwo = (t[3] * 60) + (t[4] * 1);
-        var secondsOne = (t2[3] * 60) + (t2[4] * 1);
-        
-        var f = json_data[i].date.split(/[-]/);
-        var theDay = f[2] + 11
-        console.log( theDay );
-        
-        var completionTime = (secondsOne - secondsTwo);
-        oranges2 += '{"completion_time": ' + '"' + completionTime + '"' + " , " + '"date": ' + '"' + theDay + '"' + "}, ";
-    }
-    console.log( oranges2 );
-    
+    console.log(json_data);
     
                 function InitChart() {
-                    var oranges = [
-                        {"completion_time": "400" , "date": "2911"},
-                        {"completion_time": "650" , "date": "0811"},
-                        {"completion_time": "720" , "date": "1911"},
-                        {"completion_time": "540" , "date": "1511"},
-                        {"completion_time": "600" , "date": "2911"},
-                        {"completion_time": "690" , "date": "811"},
-                        {"completion_time": "120" , "date": "3911"},
-                        {"completion_time": "540" , "date": "0511"},
-                        {"completion_time": "625" , "date": "2211"},
-                        {"completion_time": "610" , "date": "0211"},
-                        {"completion_time": "120" , "date": "2211"},
-                        {"completion_time": "140" , "date": "2511"},
-                        {"completion_time": "620" , "date": "1911"},
-                        {"completion_time": "60" , "date": "1811"},
-                        {"completion_time": "120" , "date": "1911"},
-                        {"completion_time": "540" , "date": "1511"}];
                     
-                    var vis = d3.select("#graph"),
-                        WIDTH = 500,
-                        HEIGHT = 500,
-                        MARGINS = {
-                            top: 20,
-                            right: 20,
-                            bottom: 20,
-                            left: 50
-                        },
-                        xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([60, 800]),
-                        yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([500, 3000]),
-                        xAxis = d3.svg.axis()
-                        .scale(xScale),
-                        yAxis = d3.svg.axis()
-                        .scale(yScale)
-                        .orient("left");
+                    var width = 3000,
+                        height = 600,
+                        padding = 50;
+                        
+                    // create an svg container
+                    var vis = d3.select("#graph").
+                        append("svg:svg")
+                            .attr("width", width)
+                            .attr("height", height);
+                            
+                    // define the y scale  (vertical)
+                    var yScale = d3.scale.linear()
+            	        .domain([0, 100])    // values between 0 and 100
+            		.range([height - padding, padding]);   // map these to the chart height, less padding.  
+                             //REMEMBER: y axis range has the bigger number first because the y value of zero is at the top of chart and increases as you go down.
+            		    
+                    // define the x scale (horizontal)
+                    var mindate = new Date(2017,1,1),
+                        maxdate = new Date(2017,12,31);
+                        
+                    var xScale = d3.time.scale()
+            	        .domain([mindate, maxdate])    // values between for month of january
+            		.range([padding, width - padding * 2]);   // map these the the chart width = total width minus padding at both sides
+            		    
+            	
+                    // define the y axis
+                    var yAxis = d3.svg.axis()
+                        .orient("left")
+                        .scale(yScale);
                     
-                    vis.append("svg:g")
-                        .attr("class", "x axis")
-                        .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
-                        .call(xAxis);
-                    vis.append("svg:g")
-                        .attr("class", "y axis")
-                        .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+                    // define the y axis
+                    var xAxis = d3.svg.axis()
+                        .orient("bottom")
+                        .scale(xScale);
+                        
+                    // draw y axis with labels and move in from the size by the amount of padding
+                    vis.append("g")
+                        .attr("transform", "translate("+padding+",0)")
                         .call(yAxis);
+            
+                    // draw x axis with labels and move to the bottom of the chart area
+                    vis.append("g")
+                        .attr("class", "xaxis")   // give it a class so it can be used to select only xaxis labels  below
+                        .attr("transform", "translate(0," + (height - padding) + ")")
+                        .call(xAxis);
+                        
+                    // now rotate text on x axis
+                    // solution based on idea here: https://groups.google.com/forum/?fromgroups#!topic/d3-js/heOBPQF3sAY
+                    // first move the text left so no longer centered on the tick
+                    // then rotate up to get 45 degrees.
+                   vis.selectAll(".xaxis text")  // select all the text elements for the xaxis
+                      .attr("transform", function(d) {
+                          return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height + ")rotate(-45)";
+                    });
+                    
+                    
                     var lineGen = d3.svg.line()
                         .x(function(d) {
-                            return xScale(d.completion_time);
+                            var dt = new Date(d.date)
+                            return xScale(dt);
                         })
                         .y(function(d) {
-                            return yScale(d.date);
+                            return yScale(d.completion_time);
                         })
                         .interpolate("basis");
                     vis.append('svg:path')
-                        .attr('d', lineGen(oranges))
+                        .attr('d', lineGen(json_data))
                         .attr('stroke', 'green')
                         .attr('stroke-width', 2)
                         .attr('fill', 'none');
--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -3,11 +3,26 @@ class GraphController < ApplicationController
   end
  
   def data
+    
     @mrpjob = Mrpjob.all
     @runtimes = Runtime.all
+    @array_of_times = []
+    @mrpjob.first.runtimes.each do |f|
+                            @start_time = f.start_time.strftime('%H:%M')
+                            @end_time = f.end_time.strftime('%H:%M') 
+                            @date = f.date.strftime('%Y/%m/%d')
+                            
+                            if (f.start_time > f.end_time)
+                              @completion_time = ((f.end_time - f.start_time)/60).floor + 1440 
+                            else 
+                              @completion_time = ((f.end_time - f.start_time)/60).floor
+                            end
+                            
+                            @array_of_times.push(completion_time: @completion_time, date: @date)
+                        end
     respond_to do |format|
       format.json {
-        render :json => @mrpjob.first.runtimes
+        render :json => @array_of_times
       }
     end
   end
